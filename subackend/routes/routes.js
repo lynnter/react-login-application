241c55f994d20c2f40742af54b463a29b6f271e4
Lynn Tang <tanglynn2@gmail.com>
2021-03-10 19:07:10 -0600

    hashed password

--- subackend/routes/routes.js
+++ subackend/routes/routes.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy = require('../models/SignUpModels')
+const bcrypt = require('bcrypt')
 
 //sign in 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+
+    // salt password
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(req.body.password, saltPassword)
     //when it runs will create a new instance of Schema
     const signedUpUser = new signUpTemplateCopy({
         fullName: req.body.fullName,
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: securePassword
     })
     signedUpUser.save()
     .then(data => {
